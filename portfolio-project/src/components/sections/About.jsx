ba1465eae3596d9fdf2ea459fb8ca99b2f4ac09c
eyhluh <ellavilleza22@gmail.com>
2025-03-18 03:58:04 +0800

    first commit

--- portfolio-project/src/components/sections/About.jsx
+++ portfolio-project/src/components/sections/About.jsx
@@ -23,18 +23,18 @@ export const About = () => {
         <div className="max-w-3xl mx-auto px-4">
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">About Me</h2>
 
-            <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-                <p className="text-gray-300 mb-6 text-justify">
+            <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all border hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                <p className="text-gray-300 mb-6 text-justify ">
                    I am currently a college student pursuing a Bachelor of Science in Computer Science. Alongside my studies, I am passionate about photography and graphic design, using my creativity to capture and create compelling visuals. My skills in both tech and art allow me to approach projects with a unique and innovative perspective.
                 </p>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                    <div className="roundedF-xl p-6 hover:-translate-y-1 transition-all">
                         <h3 className="text-xl font-bold mb-4">Frontend</h3>
                         <div className="flex flex-wrap gap-2">
                             {frontendSkills.map((tech, key) => (
                                 <span key={key} 
-                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(69,130,2246,0.2)] transition">
+                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(69,130,246,0.2)] transition">
                                     {tech}
                                 </span>
                             ))}
@@ -46,7 +46,7 @@ export const About = () => {
                         <div className="flex flex-wrap gap-2">
                             {backendSkills.map((tech, key) => (
                                 <span key={key} 
-                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(69,130,2246,0.2)] transition">
+                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(69,130,246,0.2)] transition">
                                     {tech}
                                 </span>
                             ))}
@@ -58,7 +58,7 @@ export const About = () => {
 {/* text-justify */}
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all border hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                     <h3 className="text-xl font-bold mb-4">🏫 Education</h3>
                     <ul className="list-disc list-inside text-gray-300 space-y-2 text-justify">
                         <li>
@@ -73,8 +73,8 @@ export const About = () => {
                     </ul>
                 </div>
 
-                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                    <h3 className="text-xl font-bold mb-4">💼 Experience</h3>
+                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all border hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                    <h3 className="text-xl font-bold mb-4">💡Experience</h3>
                     <div className="space-y-4 text-gray-300 text-justify">
                         <div>
                             <h4 className="font-semibold">FCU CCS FITC Organization</h4>
---
+++ portfolio-project/src/components/sections/Projects.jsx
@@ -0,0 +1,33 @@
+
+export const Projects = () => {
+
+    return (
+        <section id="projects" className="min-h-screen flex items-center justify-center py-20">
+
+            <div className="max-w-5xl mx-auto px-4">
+                <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">PROJECTS</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition text-justify"> 
+                        <h3 className="text-xl-bold mb-2">Cload Platform</h3>
+                        <p className="text-gray-400 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique ab. Eveniet placeat totam doloremque adipisci, eligendi unde neque, consectetur, accusamus voluptates earum incidunt ipsa sed ex aliquid esse. Quis!</p>
+                        <div>
+                            {["React", "Node.js"].map((tech, key)=> (
+                                <span key={key} 
+                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm  hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(69,130,246,0.1)] transition-all ">
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+
+                        <div className="flex justify-between items-center">
+                            <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors">View Project</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    );
+}
